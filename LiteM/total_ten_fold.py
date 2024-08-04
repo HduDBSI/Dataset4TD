1e0c3eb00bfc20d3b827b2bfb36fb1a0a937251f
HduDBSI <87598215+HduDBSI@users.noreply.github.com>
2024-08-05 00:04:18 +0800

    Update total_ten_fold.py

--- LiteM/total_ten_fold.py
+++ LiteM/total_ten_fold.py
@@ -2,14 +2,15 @@ import sys
 sys.path.append("..")
 from project_Info import *
 from LatexTable import *
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import StratifiedKFold
 from lightgbm import LGBMClassifier
 import pandas as pd
-from imblearn.over_sampling import SMOTE
 import numpy as np
-random_state = 1
+from utils import cal_metrics
+import time
 
+random_state = 1
+label_column_name = 'CommentsAssociatedLabel'
 def makeXy(granularity):
 
     X_list, y_list = [], []
@@ -28,7 +29,7 @@ def makeXy(granularity):
         else:
             X_tmp = data[block_feature_names_lowercase]
 
-        y_tmp = data['CommentsAssociatedLabel'.lower()]
+        y_tmp = data[label_column_name.lower()]
         
         X_list.append(X_tmp)
         y_list.append(y_tmp)
@@ -40,9 +41,8 @@ def makeXy(granularity):
     
     return X, y
 
-
 # ten folds cross validation
-def ten_folds_train(granularity):
+def ten_folds(granularity):
     # load dataset
     X, y = makeXy(granularity)
     
@@ -53,8 +53,9 @@ def ten_folds_train(granularity):
     precisions = []
     recalls = []
     f1_scores = []
-    feature_importances = []
-    
+    AUCs = []
+    MCCs = []
+
     X = X.fillna(-1)
     # loop for cross validation
     for train_index, test_index in skf.split(X, y):
@@ -62,69 +63,60 @@ def ten_folds_train(granularity):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
    
-        X_train_resample, y_train_resample = X_train, y_train
-        clf = LGBMClassifier(objective='binary', metric='binary_logloss', random_state=random_state, n_jobs=12)
+        clf = LGBMClassifier(random_state=random_state, n_jobs=12, n_estimators=500, learning_rate=0.05)
         
         # train 
-        clf.fit(X_train_resample, y_train_resample)
+        clf.fit(X_train, y_train)
        
         # predict
         y_pred = clf.predict(X_test)
+        y_pred_prob = clf.predict_proba(X_test)[:, 1]
 
-        # record importances
-        importance = clf.feature_importances_
-        feature_importances.append(importance)
-        
-        # calculate accuracy, precision, recall, f1-score
-        accuracy = accuracy_score(y_test, y_pred)
-        precision = precision_score(y_test, y_pred)
-        recall = recall_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred)
+        # calculate metrics
+        metrics = cal_metrics(y_test, y_pred, y_pred_prob)
 
         # save this round result
-        accuracies.append(accuracy)
-        precisions.append(precision)
-        recalls.append(recall)
-        f1_scores.append(f1)
+        accuracies.append(metrics['ACC'])
+        precisions.append(metrics['P'])
+        recalls.append(metrics['R'])
+        f1_scores.append(metrics['F1'])
+        AUCs.append(metrics['AUC'])
+        MCCs.append(metrics['MCC'])
 
     # calculate average
     mean_accuracy = sum(accuracies) / len(accuracies)
     mean_precision = sum(precisions) / len(precisions)
     mean_recall = sum(recalls) / len(recalls)
     mean_f1_score = sum(f1_scores) / len(f1_scores)
-    mean_feature_importances = np.mean(np.array(feature_importances), axis=0)
+    mean_auc = sum(AUCs) / len(AUCs)
+    mean_mcc = sum(MCCs) / len(MCCs)
+    
+    print("Mean Accuracy:{:.2f}".format(mean_accuracy))
+    print("Mean Precision:{:.2f}".format(mean_precision))
+    print("Mean Recall:{:.2f}".format(mean_recall))
+    print("Mean F1-score:{:.2f}".format(mean_f1_score))
+    print("Mean AUC: {:.2f}".format(mean_auc))
+    print("Mean MCC: {:.2f}".format(mean_mcc))
 
-    print("Mean Accuracy:{:.2%}".format(mean_accuracy))
-    print("Mean Precision:{:.2%}".format(mean_precision))
-    print("Mean Recall:{:.2%}".format(mean_recall))
-    print("Mean F1-score:{:.2%}".format(mean_f1_score))
-    return mean_precision, mean_recall, mean_f1_score, mean_feature_importances
+    return mean_precision, mean_recall, mean_f1_score, mean_auc, mean_mcc
 
 def normalize_list(lst):
     min_value = min(lst)
     max_value = max(lst)
     return [(x - min_value) / (max_value - min_value) for x in lst]
 
-
-import time
 t = time.time()
-matrix = []
-# granularities.remove('class')
-# granularities.remove('method')
-importances = []
+latex_matrix = []
+
 for granularity in granularities:
     print('===='+granularity+'====')
-    p, r, f, i = ten_folds_train(granularity)
-    line = [p, r, f]
-    importances.append(i)
-    matrix.append(line)
+    p, r, f, auc, mcc = ten_folds(granularity)
+    latex_line = [p, r, f, auc, mcc]
+    latex_matrix.append(latex_line)
+
 print(time.time()-t)
 
-
-# for importance in importances:
-#     print(importance)
-
-avgs = avgEachColumn(matrix)
-matrix = insertRow(matrix, avgs, len(matrix))
-matrix = insertColumn(matrix, ['File', 'Class', 'Method', 'Block', 'Average'], 0)
-writeTable(matrix, 'results/total_ten_fold.txt')
+avgs = avgEachColumn(latex_matrix)
+latex_matrix = insertRow(latex_matrix, avgs, len(latex_matrix))
+latex_matrix = insertColumn(latex_matrix, ['File', 'Class', 'Method', 'Block', 'Average'], 0)
+writeTable(latex_matrix, 'results/total_ten_fold.txt')
