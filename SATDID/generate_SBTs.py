1d6cf45358a3890d9f255943a51d7625176c8f62
HduDBSI <87598215+HduDBSI@users.noreply.github.com>
2024-08-04 23:31:00 +0800

    Update generate_SBTs.py

--- SATDID/generate_SBTs.py
+++ SATDID/generate_SBTs.py
@@ -1,13 +1,14 @@
 import os
 import sys
-from time import time
+import time
 sys.path.append("../") 
-from project_Info import projects
+from project_Info import projects, project_names
+import numpy as np
 
 def generate_SBT(project):
-    srcDir = 'data/java_files/' + project
+    srcDir = 'data\\java_files\\' + project
     jdk = 'java'
-    outfile = 'data/' + project + '.csv'
+    outfile = 'data\\' + project + '.csv'
     jar = 'SBT.jar'
     cmd = [jdk, '-jar', jar, srcDir, outfile]
     cmd = " ".join(cmd)                                  
@@ -15,7 +16,16 @@ def generate_SBT(project):
     e = os.popen(cmd)
     e.close()
 
-t = time()
+times = []
 for project in projects:
+    t = time.time()
     generate_SBT(project)
-print('cost time:', time()-t)
+    times.append(time.time() - t)
+
+with open('results/time2.txt', 'w') as f:
+    for t, project in zip(times, project_names):
+        f.write("{}\t{:.2f}\n".format(project, t))
+    f.write("Median\t{:.2f}\n".format(np.median(times)))
+    f.write("Total\t{:.2f}\n".format(np.sum(times)))
+        
+print('cost time:', sum(times))
