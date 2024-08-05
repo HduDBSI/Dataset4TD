755072d5dd0eed54860dc3c830532e1ef45ac13f
HduDBSI <87598215+HduDBSI@users.noreply.github.com>
2024-08-05 10:38:30 +0800

    Update classAndMethodMetrics.py

--- metrics tool/classAndMethodMetrics.py
+++ metrics tool/classAndMethodMetrics.py
@@ -1,13 +1,14 @@
 import sys
 import os
-sys.path.append("../") 
-from project_Info import projects
-from time import time
+import numpy as np
+sys.path.append("../../") 
+from project_Info import projects, project_names
+import time
 
 def extractMetrics(project):
-    srcfile = '..\\projects\\' + project
+    srcfile = '..\\..\\projects\\' + project
     jdk = 'jdk11\\bin\\java.exe'
-    outfile = '..\\metrics\\' + project + '-'
+    outfile = '..\\..\\metrics\\' + project + '-'
     jar = 'ck.jar'
     parameters = 'True 0 False'
     cmd = [jdk, '-jar', jar, srcfile, parameters, outfile]
@@ -16,10 +17,15 @@ def extractMetrics(project):
     e = os.popen(cmd)
     e.close()
 
-t = time()
+
+times = []
 granularities = ["class", "method"]
 for project in projects:
+    t = time.time()
     extractMetrics(project)
-print('cost time:', time()-t)
-
-
+    times.append(time.time() - t)
+with open('time-ck.txt', 'w') as f:
+    for t, project in zip(times, project_names):
+        f.write("{}\t{:.2f}\n".format(project, t))
+    f.write("Median\t{:.2f}\n".format(np.median(times)))
+    f.write("Total\t{:.2f}\n".format(np.sum(times)))
