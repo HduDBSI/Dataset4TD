551c33565a34c4df0ec27ed689226b320333d7a9
HduDBSI <87598215+HduDBSI@users.noreply.github.com>
2024-08-05 10:33:37 +0800

    Update split.py

--- data_collection/split.py
+++ data_collection/split.py
@@ -19,46 +19,72 @@ def pad(s:str)->str:
     return s
 
 # 逐行读取文件内容
-dic1, dic2 = {}, {}
+dic1, dic2, dic3 = {}, {}, {}
 median = {}
 for line in file:
     # 处理每行数据
     line = line.strip()  # 移除行尾换行符和空格
     data = line.split('&')  # 按&符号分割数据
     dic1[data[0]] = {
-        '#file': data[1],
-        '#fwc': num2str( str2num(data[5]) + str2num(data[6]), 5),
-        'pfwc': pad("{:.2%}".format((str2num(data[5]) + str2num(data[6])) / str2num(data[1]) )),
+        'f#cs': data[1],
+        'f#ccs': num2str( str2num(data[5]) + str2num(data[6]), 5),
+        'f%%ccs': pad("{:.2%}".format((str2num(data[5]) + str2num(data[6])) / str2num(data[1]) )),
 
-        '#class': data[2],
-        '#cwc': num2str(str2num(data[7]) + str2num(data[8]), 5),
-        'pcwc': pad("{:.2%}".format((str2num(data[7]) + str2num(data[8])) / str2num(data[2]) )),
+        'c#cs': data[2],
+        'c#ccs': num2str(str2num(data[7]) + str2num(data[8]), 5),
+        'c%%ccs': pad("{:.2%}".format((str2num(data[7]) + str2num(data[8])) / str2num(data[2]) )),
 
-        '#method': data[3],
-        '#mwc': num2str( str2num(data[9]) + str2num(data[10]), 6),
-        'pmwc': pad("{:.2%}".format((str2num(data[9]) + str2num(data[10])) / str2num(data[3]) )),
+        'm#cs': data[3],
+        'm#ccs': num2str( str2num(data[9]) + str2num(data[10]), 6),
+        'm%%ccs': pad("{:.2%}".format((str2num(data[9]) + str2num(data[10])) / str2num(data[3]) )),
 
-        '#block': data[4],
-        '#bwc': num2str( str2num(data[11]) + str2num(data[12]), 6),
-        'pbwc': pad("{:.2%}".format((str2num(data[11]) + str2num(data[12])) / str2num(data[4]) )),
+        'b#cs': data[4],
+        'b#ccs': num2str( str2num(data[11]) + str2num(data[12]), 6),
+        'b%%ccs': pad("{:.2%}".format((str2num(data[11]) + str2num(data[12])) / str2num(data[4]) )),
     }
 
     dic2[data[0]] = {
-        '#fwc': num2str(str2num(data[5]) + str2num(data[6]), 5),
-        '#fwc1': data[5],
-        'pfwc1': pad("{:.2%}".format(str2num(data[5]) / (str2num(data[5]) + str2num(data[6])) )),
+        'f#ccs': num2str(str2num(data[5]) + str2num(data[6]), 5),
+        'f#tdwa': data[5],
+        'f%%tdwa': pad("{:.2%}".format(str2num(data[5]) / (str2num(data[5]) + str2num(data[6])) )),
 
-        '#cwc': num2str(str2num(data[7]) + str2num(data[8]), 5),
-        '#cwc1': data[7],
-        'pcwc1': pad("{:.2%}".format(str2num(data[7]) / (str2num(data[7]) + str2num(data[8])) )),
+        'c#ccs': num2str(str2num(data[7]) + str2num(data[8]), 5),
+        'c#tdwa': data[7],
+        'c%%tdwa': pad("{:.2%}".format(str2num(data[7]) / (str2num(data[7]) + str2num(data[8])) )),
 
-        '#mwc': num2str(str2num(data[9]) + str2num(data[10]), 6),
-        '#mwc1': data[9],
-        'pmwc1': pad("{:.2%}".format(str2num(data[9]) / (str2num(data[9]) + str2num(data[10])) )),
+        'm#ccs': num2str(str2num(data[9]) + str2num(data[10]), 6),
+        'm#tdwa': data[9],
+        'm%%tdwa': pad("{:.2%}".format(str2num(data[9]) / (str2num(data[9]) + str2num(data[10])) )),
 
-        '#bwc': num2str(str2num(data[11]) + str2num(data[12]), 6),
-        '#bwc1': data[11].replace('\\\\',''),
-        'pbwc1': pad("{:.2%}".format(str2num(data[11]) / (str2num(data[11]) + str2num(data[12])) )),
+        'b#ccs': num2str(str2num(data[11]) + str2num(data[12]), 6),
+        'b#tdwa': data[11].replace('\\\\',''),
+        'b%%tdwa': pad("{:.2%}".format(str2num(data[11]) / (str2num(data[11]) + str2num(data[12])) )),
+    }
+
+    dic3[data[0]] = {
+        'f#cs': data[1],
+        'f#ccs': num2str( str2num(data[5]) + str2num(data[6]), 5),
+        'f%%ccs': pad("{:.2%}".format((str2num(data[5]) + str2num(data[6])) / str2num(data[1]) )),
+        'f#tdwa': data[5],
+        'f%%tdwa': pad("{:.2%}".format(str2num(data[5]) / (str2num(data[5]) + str2num(data[6])) )),
+
+        'c#cs': data[2],
+        'c#ccs': num2str( str2num(data[7]) + str2num(data[8]), 5),
+        'c%%ccs': pad("{:.2%}".format((str2num(data[7]) + str2num(data[8])) / str2num(data[2]) )),
+        'c#tdwa': data[7],
+        'c%%tdwa': pad("{:.2%}".format(str2num(data[7]) / (str2num(data[7]) + str2num(data[8])) )),
+
+        'm#cs': data[3],
+        'm#ccs': num2str( str2num(data[9]) + str2num(data[10]), 5),
+        'm%%ccs': pad("{:.2%}".format((str2num(data[9]) + str2num(data[10])) / str2num(data[3]) )),
+        'm#tdwa': data[9],
+        'm%%tdwa': pad("{:.2%}".format(str2num(data[9]) / (str2num(data[9]) + str2num(data[10])) )),
+
+        'b#cs': data[4],
+        'b#ccs': num2str( str2num(data[11]) + str2num(data[12]), 5),
+        'b%%ccs': pad("{:.2%}".format((str2num(data[11]) + str2num(data[12])) / str2num(data[4]) )),
+        'b#tdwa': data[11].replace('\\\\',''),
+        'b%%tdwa': pad("{:.2%}".format(str2num(data[11]) / (str2num(data[11]) + str2num(data[12])) )),
     }
 
 
@@ -78,6 +104,12 @@ with open('t2.txt', 'w') as f:
         line = line.replace('%', '\%')
         f.write(line + "\n\hline\n")
 
+with open('t3.txt', 'w') as f:
+    for key, value in dic3.items():
+        line = key + '&' + '&'.join(value.values()) + '\\\\'
+        line = line.replace('%', '\%')
+        f.write(line + "\n\hline\n")
+
 with open('t1.csv', 'w') as f:
     for key, value in dic1.items():
         tmp = value.values()
@@ -87,6 +119,13 @@ with open('t1.csv', 'w') as f:
 
 with open('t2.csv', 'w') as f:
     for key, value in dic2.items():
+        tmp = value.values()
+        tmp = [t.replace('\\enspace', '').replace(',', '').replace('\;','') for t in tmp]
+        line = key + ',' + ','.join(tmp)
+        f.write(line + "\n")
+
+with open('t3.csv', 'w') as f:
+    for key, value in dic3.items():
         tmp = value.values()
         tmp = [t.replace('\\enspace', '').replace(',', '').replace('\;','') for t in tmp]
         line = key + ',' + ','.join(tmp)
